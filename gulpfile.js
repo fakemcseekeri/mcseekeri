ef5d94c914699df7e440d54a58f0a79d371b6163
MCSeekeri <mcseekeri@outlook.com>
2021-02-15 11:34:14 +0800

    小更新

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const webp = require('gulp-webp');
-const rename = require('gulp-rename')
-gulp.src('./image/*.{png,jpg}')
-  .pipe(rename(function (path) {
-    path.extname += '.webp';
-  })
-  .pipe(webp())
-  .pipe(gulp.dest('./')));
